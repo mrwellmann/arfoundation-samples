d7bcabdc0fd0c488a0c3d44018b1d50edc9f1162
Markus Wellmann <markus.wellmann@firedrill.de>
2023-03-28 16:55:15 +0200

    Changed Settings layout, Changed visualization of Points and Plains, made Points and Plains hide able
    Note: There is a bug. In the editor plains have be be deactivated from the beginning and placing on them does not work.

--- Assets/01_Samaritan/Scripts/Settings/MainControls.cs
+++ Assets/01_Samaritan/Scripts/Settings/MainControls.cs
@@ -1,11 +1,41 @@
 using System;
 using System.Collections;
 using System.Collections.Generic;
+using System.Linq;
+using Unity.XR.CoreUtils;
+using UnityEditor;
 using UnityEngine;
+using UnityEngine.UI;
+using UnityEngine.XR.ARFoundation;
 
 public class MainControls : MonoBehaviour
 {
-    internal void Init()
+    [SerializeField]
+    private Toggle _planes;
+    [SerializeField]
+    private Toggle _featurePoints;
+
+    public void Init()
     {
+        _planes.onValueChanged.AddListener(EnablePlanes);
+        _featurePoints.onValueChanged.AddListener(EnableFeaturePoints);
+
+        EnablePlanes(_planes.isOn);
+        EnableFeaturePoints(_featurePoints.isOn);
+    }
+
+    private void EnableFeaturePoints(bool isOn)
+    {
+        ARPointCloudManager aRPointCloudManager = XROrigin.FindObjectOfType<ARPointCloudManager>();
+
+        aRPointCloudManager.SetTrackablesActive(isOn);
+        aRPointCloudManager.enabled = isOn;
+    }
+
+    private void EnablePlanes(bool isOn)
+    {
+        ARPlaneManager aRPlaneManager = XROrigin.FindObjectOfType<ARPlaneManager>();
+        aRPlaneManager.SetTrackablesActive(isOn);
+        aRPlaneManager.enabled = isOn;
     }
 }
