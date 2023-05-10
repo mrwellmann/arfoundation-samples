8b9537d24d5e19764a2aa3d7c85e94abf083f781
Markus Wellmann <mrwellmann@gmail.com>
2023-05-10 15:34:54 +0200

    Fixed Back Button and Main Menu Errors

--- Assets/Scripts/UI/BackButton.cs
+++ Assets/Scripts/UI/BackButton.cs
@@ -6,20 +6,21 @@ namespace UnityEngine.XR.ARFoundation.Samples
     public class BackButton : MonoBehaviour
     {
         [SerializeField]
-        GameObject m_BackButton;
+        private GameObject m_BackButton;
+
         public GameObject backButton
         {
             get => m_BackButton;
             set => m_BackButton = value;
         }
 
-        void Start()
+        private void Start()
         {
             if (Application.CanStreamedLevelBeLoaded("Menu"))
                 m_BackButton.SetActive(true);
         }
 
-        void Update()
+        private void Update()
         {
             if (Keyboard.current != null && Keyboard.current.escapeKey.wasPressedThisFrame)
                 BackButtonPressed();
